8aadbd67f013b60516755cb8e267132cc4c6c88f
tsubasa2911 <randoctor11@gmail.com>
2024-10-23 12:03:29 +0900

    ファイル修正

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -10,6 +10,9 @@ $score8 = 95;
 $score9 = 30;
 $score10 = 60;
 
-echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 +$score10 ) / 10;
+$sum_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 +$score10;
+
+$average_score =$sum_score / 10;
+echo $average_score;
 
 ?>
